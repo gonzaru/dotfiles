494273536e89ebb19b7159259bf8c2cce114dff5
Ruben Gonzalez <neorubenx@gmail.com>
2022-06-07 10:11:40 +0200

    Update

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,6 +1,6 @@
 ;; reload configuration
 ;; M-x: load-file ~/.emacs.d/init.el
-;; open ~/.emacs.d/init.d & M-x: eval-buffer
+;; M-x: eval-buffer (with init.el opened)
 
 ;; disable welcome message
 (setq inhibit-startup-screen t)
@@ -11,8 +11,8 @@
 ;; gui
 (if (display-graphic-p)
     (progn
-      ;; disable menu bar
-      (menu-bar-mode 1)
+      ;; enable menu bar
+      (menu-bar-mode t)
       ;; disable tool bar
       (tool-bar-mode -1) 
       ;; disable sroll bar
@@ -21,6 +21,7 @@
       ;; size value is 1/10pt, so 100 = 10pt
       (set-face-attribute 'default nil :height 140))
   ;; non gui
+  ;; disable menu bar
   (menu-bar-mode -1))
 
 ;; don't create backup files file~
@@ -33,28 +34,28 @@
 (setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
 
 ;; show column number
-(column-number-mode 1)
+(column-number-mode t)
 
 ;; matching parentheses, braces, etc.
-(show-paren-mode 1)
+(show-paren-mode t)
 
 ;; highlighting the current line
-;; (global-hl-line-mode 1)
+;; (global-hl-line-mode t)
 
 ;; wrap properly at word boundaries
-;; (global-visual-line-mode 1)
+;; (global-visual-line-mode t)
 
 ;; show numbers
-(global-display-line-numbers-mode 1)
+(global-display-line-numbers-mode t)
 
 ;; relative numbers
 (setq display-line-numbers-type 'relative)
 
 ;; auto update the current file when is changed externally 
-(global-auto-revert-mode 1)
+(global-auto-revert-mode t)
 
 ;; delete using C-h (conflicts with help)
-;; normal-erase-is-backspace-mode 1)
+;;(normal-erase-is-backspace-mode t)
 (global-set-key (kbd "C-h") 'delete-backward-char)
 
 ;; alternative C-h map
